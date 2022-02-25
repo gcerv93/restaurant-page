378fe59bf65b13f8c0fa468c94a11aef1cca8fb3
gcerv93 <gcerv93@protonmail.com>
2022-02-25 14:07:37 -0600

    add watch and build scripts

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("alert('Hello webpack');\n\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
+eval("\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
 
 /***/ })
 
